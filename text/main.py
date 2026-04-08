7105a834218fc57526bf04af0354fc350fc2f34f
zhenya194 <zhenya.pro1753@gmail.com>
2026-04-08 12:16:06 +0300

    Update text script

--- text/main.py
+++ text/main.py
@@ -1,12 +1,14 @@
 while True:
     text:str = input("Write your text: ")
+    if text.lower() == "exit" or text.lower() == "stop":
+        break
     style:str = input("Which style do you want to do on text(lower or upper): ")
+    if style.lower() == "exit" or style.lower() == "stop":
+        break
     if style.lower() == "lower":
         print(f"\n\n{text.lower()}\n")
-        break
     elif style.lower() == "upper":
         print(f"\n\n{text.upper()}\n")
-        break
     else:
         print("Style is not correct")
         continue
